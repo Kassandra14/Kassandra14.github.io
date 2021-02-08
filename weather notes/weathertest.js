7be6030567fb4ce0dfca08b05edb116551acdc33
Kas <ksixbones@gmail.com>
2021-02-07 19:52:52 -0700

    second

--- weather notes/weathertest.js
+++ weather notes/weathertest.js
@@ -1,15 +1,40 @@
+var userFormEl = document.querySelector("#city");
+var currentWeatherContainerEl = document.querySelector("#current-weather");
+var forecastWeatherEl = document.querySelector("#forecast-weather");
+
+var requestOptions = {
+method: 'GET',
+redirect: 'follow'
+};
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid=cfd2430c3ee19405f5b8a3b9dd3ab7f3", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 
 
-// var requestOptions = {
-// method: 'GET',
-// redirect: 'follow'
-// };
+var getTheCityForecast = function(city) {
+    var forecastApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=cfd2430c3ee19405f5b8a3b9dd3ab7f3&unit=imperial"; 
+    
+    var requestForecast = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+    
+    fetch(forecastApi)
+    .then(response => response.json())
+    .then(data => {
+        var nameEl = document.createElement('h1')
+        nameEl.innerHTML = data.city.name
+        forecastWeatherEl.appendChild(nameEl)
+        var nameEl = document.createElement('h1')
+        nameEl.innerHTML = data.city.name
+        forecastWeatherEl.appendChild(nameEl)
+    })
 
-// fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid=cfd2430c3ee19405f5b8a3b9dd3ab7f3", requestOptions)
-//     .then(response => response.json())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
+};
 
+getTheCityForecast('New York');
 
     var currentWeatherEl = document.getElementById('current-weather');
     var fetchButton = document.getElementById('fetch-button');
@@ -19,7 +44,7 @@
 
     function getApi(London) {
       // fetch results for a city
-    var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=cfd2430c3ee19405f5b8a3b9dd3ab7f3';
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + value + "&unit=imperial&appid=cfd2430c3ee19405f5b8a3b9dd3ab7f3";
     
     fetch(requestUrl)
         .then(function (response) {
